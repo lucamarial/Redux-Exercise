36d1acd80f54addd8c9ed0c89c581eada48aac6d
luca <l.lobacher@web.de>
2020-02-03 15:34:35 +0100

    Creates form with input field and button

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -4,6 +4,15 @@ const Form = () => {
   return (
     <>
       <h2>Add a new article</h2>
+
+      <form>
+        <input
+          type='text'
+          id='title'
+          placeholder='Title'
+        />
+        <button type='submit'>Save</button>
+      </form>
     </>
   )
 }
