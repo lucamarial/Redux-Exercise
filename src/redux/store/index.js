1cc91b8d38b845d0aef5b6791bbb73a6542a3ffc
luca <l.lobacher@web.de>
2020-02-03 18:43:53 +0100

    Adds forbiddenWordsMiddelware functionality

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers/index'
+import { forbiddenWordsMiddelware } from '../middelware/index'
 
-const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer,
+  applyMiddleware(forbiddenWordsMiddelware)
+)
 
 export default store
