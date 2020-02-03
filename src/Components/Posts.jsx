8f840ac0744c0dc375dac47a5bf68935580c6c00
luca <l.lobacher@web.de>
2020-02-03 20:20:03 +0100

    Gets remoteArticles state and maps over array

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -9,12 +9,23 @@ const Posts = props => {
 
   return (
     <>
-      
+      <h2>API posts</h2>
+      <ul>
+        {props.remoteArticles.map(article => (
+          <li key={article.id}>{article.title}</li>
+        ))}
+      </ul>
     </>
   )
 }
 
+const mapStateToProps = state => {
+  return {
+    remoteArticles: state.remoteArticles.slice(0, 10)
+  }
+}
+
 export default connect(
-  null,
+  mapStateToProps,
   { getData }
 )(Posts)
