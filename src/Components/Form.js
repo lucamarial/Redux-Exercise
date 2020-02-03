1c328829e3d3ea3b2c07ef1d17f4cf47e90693ca
luca <l.lobacher@web.de>
2020-02-03 15:45:33 +0100

    Adds onChange handler and sets title state

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+  const [title, setTitle] = useState('')
+
+  const handleChange = e => {
+    setTitle(e.target.value)
+  }
+
   return (
     <>
       <h2>Add a new article</h2>
@@ -10,6 +16,8 @@ const Form = () => {
           type='text'
           id='title'
           placeholder='Title'
+          value={title}
+          onChange={handleChange}
         />
         <button type='submit'>Save</button>
       </form>
