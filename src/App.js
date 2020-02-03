eaaba5dca9810f70edcd72e44a12d4f4bd641dee
luca <l.lobacher@web.de>
2020-02-03 15:17:45 +0100

    Creates Form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import List from './Components/List'
+import Form from './Components/Form'
 
 const App = () => {
   return (
     <>
       <List />
+      <Form />
     </>
   )
 }
---
+++ src/Components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <>
+      <h2>Add a new article</h2>
+    </>
+  )
+}
+
+export default Form
