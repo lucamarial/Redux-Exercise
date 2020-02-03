8a78aaf27ea7d6a0d1f0c7174fdbb7d0cc5f5cc7
luca <l.lobacher@web.de>
2020-02-03 14:50:32 +0100

    Adds Provider to connect with Redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { Provider } from 'react-redux'
+import store from './redux/store/index'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'))
 
-serviceWorker.unregister();
+serviceWorker.unregister()
