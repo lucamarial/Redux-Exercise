8c5f1741c182f8e8474cb2549e352d9e0f4bd2c1
luca <l.lobacher@web.de>
2020-02-03 19:23:12 +0100

    Creates getData action to fetch data from API

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,3 +3,14 @@ import { ADD_ARTICLE } from '../constants/action-types'
 export const addArticle = (payload) => {
   return { type: ADD_ARTICLE, payload }
 }
+
+export const getData = () => {
+  return fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(json => {
+      return {
+        type: 'DATA_LOADED',
+        payload: json
+      }
+    })
+}
